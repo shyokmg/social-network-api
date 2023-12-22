64604e18afa6af38b2fc023ee7f97d725231ad6a
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-22 16:41:59 +1100

    Routes - added userRoutes.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
