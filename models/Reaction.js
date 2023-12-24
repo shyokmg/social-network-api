77edc2c29eb46e097988863ac474576bc43f2d43
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-24 11:04:24 +1100

    Models - Updated Reaction.js due to type on minLength

--- models/Reaction.js
+++ models/Reaction.js
@@ -9,7 +9,7 @@ const reactionSchema = new Schema(
     reactionBody: {
       type: String,
       required: true,
-      minlength: 280,
+      maxlength: 280,
     },
     username: {
         type: String,
